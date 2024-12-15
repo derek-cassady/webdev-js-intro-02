6a15b344c5bf515d4778b71b37354e2b1f55381a
derek-cassady <derek@cassadyweb.com>
2024-12-15 16:43:49 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,33 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Get back to the gym, stop wasting the membership and money!";
+const currentYear = "2024";
+const willMeetResolution = "2026"
 
 function updateYear() {
     // Update this function
-
+    yearElement.textContent = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
-
+    resolutionElement.textContent = resolution;
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.textContent = willMeetResolution;
 }
 
 function render() {
     // Update this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,70 @@
 // that are not declared
 "use strict"
 
+/*
+const myName = "Derek";
+console.log(myName)             // Output: "Dan"
+console.log(typeof myName)      // Output: "string"
+*/
+
+/*
+const numbers = 1234;
+console.log(numbers)                // Output: 1
+console.log(typeof numbers);        // Output: "number"
+*/
+
+/*
+let isStudent = true;            
+console.log(isStudent)           // Output: true
+console.log(typeof isStudent)    // Output: "boolean"
+*/
+
+/*
+let isMentor = false;
+console.log(isMentor)           // Output: false
+console.log(typeof isMentor)    // Output: "boolean"
+*/
+
+/*
+let theRiddler;
+console.log(theRiddler)         // Output: undefined
+console.log(typeof theRiddler)  // Output: undefined
+*/
+
+/*
+let careGiven = null;
+console.log(careGiven)         // Output: null
+console.log(typeof careGiven)  // Output: object
+*/
+
+/*
+//Had to use GPT to figure out the 'n' at the end
+const bigNumber = 123456789012345678901234567890n;
+console.log(bigNumber);         // Output: object
+console.log(typeof bigNumber)   // Output: bigint
+*/
+
+/*
+let age;
+let weight;
+let height;
+let neckSize;
+
+let derek = {
+    age: 41,
+    weight: 265,
+    height: `5' - 9"`,
+    neckSize: 19.75
+}
+
+console.log(age)                    // Output: undefined
+console.log(typeof age)             // Output: undefined
+console.log(weight)                 // Output: undefined
+console.log(typeof weight)          // Output: undefined
+console.log(height)                 // Output: undefined
+console.log(typeof height)          // Output: undefined
+console.log(neckSize)               // Output: undefined
+console.log(typeof neckSize)        // Output: undefined
+console.log(derek)                  // Output: { age: 41, weight: 265, height: `5' - 9"`, neckSize: 19.75 }
+console.log(typeof derek)           // Output: object
+*/
